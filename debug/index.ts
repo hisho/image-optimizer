a8fbbe5dc25abfa56dd61d6f72a5f7ba3bc57b49
hisho <hisho.web@gmail.com>
2022-07-22 03:41:06 +0900

    🔥 不要な余白の削除

--- debug/index.ts
+++ debug/index.ts
@@ -1,5 +1,5 @@
 import { exec } from 'child_process'
 
-exec(`cd debug/withNext &&  node ./../../bin/index.js`, (_, stdout, stderr) =>
+exec(`cd debug/withNext && node ./../../bin/index.js`, (_, stdout, stderr) =>
   console.log(stdout, stderr)
 )
